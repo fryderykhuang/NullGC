a20e9bc9b05f168075c405334b90242ebc201306
Fryderyk Huang <fryderyk.huang@gmail.com>
2024-01-20 08:29:43 +0800

    fix publish nuget

--- scripts/PublishNuget.ps1
+++ scripts/PublishNuget.ps1
@@ -2,7 +2,7 @@
 Push-Location $SolutionDir
 foreach ($project in $Projects)
 {
-    dotnet nuget push ".\artifacts\$project.$Env:GitVersion_NuGetVersionV2.nupkg" --skip-duplicate --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+    dotnet nuget push (Join-Path $ArtifactsDir $project.$Env:GitVersion_NuGetVersionV2.nupkg) --skip-duplicate --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
 
 Pop-Location
