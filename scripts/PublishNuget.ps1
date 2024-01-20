832107b30c3ed50a9eedfdbe05adcefd5ad255f5
Fryderyk Huang <fryderyk.huang@gmail.com>
2024-01-20 08:52:33 +0800

    fix publish nuget. +semver:patch

--- scripts/PublishNuget.ps1
+++ scripts/PublishNuget.ps1
@@ -1,8 +1,7 @@
 . $PSScriptRoot\Variables.ps1
 Push-Location $SolutionDir
-foreach ($project in $Projects)
-{
-    dotnet nuget push (Join-Path $ArtifactsDir $project.$Env:GitVersion_NuGetVersionV2.nupkg) --skip-duplicate --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+foreach ($project in $Projects) {
+    dotnet nuget push (Join-Path $ArtifactsDir $project.$Env:GitVersion_NuGetVersionV2.nupkg | Resolve-Path) --skip-duplicate --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
 
 Pop-Location
