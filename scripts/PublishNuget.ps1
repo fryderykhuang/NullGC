f2ac47154a902189a0e06aaa5fea5a36f4db8893
Fryderyk Huang <fryderyk.huang@gmail.com>
2024-01-28 04:51:38 +0800

    fix NuGet publish of analyzer packages

--- scripts/PublishNuget.ps1
+++ scripts/PublishNuget.ps1
@@ -1,6 +1,9 @@
 . $PSScriptRoot\Variables.ps1
 Push-Location $SolutionDir
 foreach ($project in $NuGetProjects) {
+    if ($project -eq 'NullGC.Analyzer.Package') {
+        $project = 'NullGC.Analyzer'
+    }
     dotnet nuget push (Join-Path $ArtifactsDir "${project}.${Env:GitVersion_NuGetVersionV2}.nupkg" | Resolve-Path) --source 'https://api.nuget.org/v3/index.json' --api-key $Env:NUGET_API_KEY --skip-duplicate
 }
 
